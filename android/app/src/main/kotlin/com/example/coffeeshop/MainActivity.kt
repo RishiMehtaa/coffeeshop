54282f1ccdc52a59c243def6870eebc207cc0936
RishiMehtaa <merit1607@gmail.com>
2024-08-23 13:27:42 +0530

    firebase done

--- android/app/src/main/kotlin/com/example/coffeeshop/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coffeeshop/MainActivity.kt
@@ -3,3 +3,9 @@ package com.example.coffeeshop
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+@Override
+fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // Ensure Firebase is initialized
+    FirebaseApp.initializeApp(this)
+}
